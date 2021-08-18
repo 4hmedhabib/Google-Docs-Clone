0d12bd1928024685ca51441485e789eda4afde7b
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-08-18 20:51:20 +0300

    modified header components

---
+++ components/Header/Header.js
@@ -0,0 +1,44 @@
+import Button from '@material-tailwind/react/Button';
+import Icon from '@material-tailwind/react/Icon';
+
+const Header = () => {
+	return (
+		<div className="flex items-center sticky top-0 z-50 px-4 py-2 shadow-md bg-white">
+			<Button
+				color="gray"
+				buttonType="outline"
+				rounded={true}
+				iconOnly={true}
+				ripple="dark"
+				className="md:inline-flex h-20 w-20 border-0"
+			>
+				<Icon name="menu" size="3xl" />
+			</Button>
+			<Icon name="description" size="5xl" color="blue" />
+			<h1 className="md:inline-flex  ml-2 text-gray-700 text-2xl">Docs</h1>
+			<div className="mx-5 md:mx-20  flex flex-grow items-center px-5 py-2 bg-gray-100 rounded-lg text-gray-600 focus-within:text-gray-600 focus-within:shadow-md">
+				<Icon name="search" size="3xl" color="gray" />
+				<input type="text" placeholder="Search" className="flex-grow px-5 bg-transparent outline-none" />
+			</div>
+			<Button
+				color="gray"
+				buttonType="outline"
+				rounded={true}
+				iconOnly={true}
+				ripple="dark"
+				className="md:inline-flex h-20 w-20 border-0"
+			>
+				<Icon name="apps" size="3xl" color="gray" />
+			</Button>
+
+			<img
+				loading="lazy"
+				className="cursor-pointer h-12 w-12 rounded-full ml-2"
+				src="https://lh3.googleusercontent.com/ogw/ADea4I5XXQw9-oOrrtERbv1MF2z_VLB3qDH4hVXzW6Bf=s83-c-mo"
+				alt=""
+			/>
+		</div>
+	);
+};
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import '@material-tailwind/react/tailwind.css';
+import Head from 'next/head';
+import { Fragment } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Fragment>
+			<Head>
+				<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+			</Head>
+			<Component {...pageProps} />
+		</Fragment>
+	);
 }
 
-export default MyApp
+export default MyApp;
